42988b0ee35ca52e701d11b3de77c02953306ded
hautran <tranleconghau772001@gmail.com>
2024-06-29 21:56:06 +0700

    update server

--- p2p/server.go
+++ p2p/server.go
@@ -1,9 +1,13 @@
 package p2p
 
 import (
+	"fmt"
 	"net"
 	"sync"
 )
+
+type TCPTransport struct {
+}
 
 type Peer struct {
 	conn net.Conn
@@ -15,9 +19,11 @@ type ServerConfig struct {
 
 type Server struct {
 	ServerConfig
-	mu      sync.Mutex
-	peers   map[net.Addr]*Peer
-	addPeer chan *Peer
+
+	listener net.Listener
+	mu       sync.Mutex
+	peers    map[net.Addr]*Peer
+	addPeer  chan *Peer
 }
 
 func NewServer(cfg ServerConfig) *Server {
@@ -30,10 +36,53 @@ func NewServer(cfg ServerConfig) *Server {
 
 func (s *Server) Start() {
 	go s.loop()
+
+	if err := s.listen(); err != nil {
+		panic(err)
+	}
+
+	go s.acceptLoop()
+}
+
+func (s *Server) handleConn(conn net.Conn) {
+	buf := make([]byte, 1024)
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			break
+		}
+
+		fmt.Println(string(buf[:n]))
+	}
+}
+
+func (s *Server) acceptLoop() {
+	for {
+		conn, err := s.listener.Accept()
+		if err != nil {
+			panic(err)
+		}
+		go s.handleConn(conn)
+	}
+}
+
+func (s *Server) listen() error {
+	ln, err := net.Listen("tcp", s.ListenAddr)
+	if err != nil {
+		return (err)
+	}
+
+	s.listener = ln
+
+	return nil
 }
 
 func (s *Server) loop() {
 	for {
-		select {}
+		select {
+		case peer := <-s.addPeer:
+			fmt.Printf("new player connected %s", peer.conn.RemoteAddr())
+			s.peers[peer.conn.RemoteAddr()] = peer
+		}
 	}
 }
