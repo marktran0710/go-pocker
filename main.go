24cd000a86e7bc42795a08890dd1f85a23f820b3
hautran <tranleconghau772001@gmail.com>
2024-06-30 23:42:31 +0700

    create new server in main

--- main.go
+++ main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/marktran77/go-pocker/deck"
+	"github.com/marktran77/go-pocker/p2p"
 )
 
 func main() {
 
-	for i := 0; i < 10; i++ {
-		d := deck.New()
-		fmt.Println(d)
-		fmt.Println()
+	// for i := 0; i < 10; i++ {
+	// 	d := deck.New()
+	// 	fmt.Println(d)
+	// 	fmt.Println()
+	//}
+
+	cfg := p2p.ServerConfig{
+		ListenAddr: ":3000",
 	}
+	server := p2p.NewServer(cfg)
+
+	server.Start()
 
 }
