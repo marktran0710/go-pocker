9915067774606e01f68319142e53f10a7260b640
hautran <tranleconghau772001@gmail.com>
2024-07-02 07:32:16 +0700

    update server

--- p2p/server.go
+++ p2p/server.go
@@ -13,6 +13,11 @@ type Peer struct {
 	conn net.Conn
 }
 
+func (p *Peer) Send(b []byte) error {
+	_, err := p.conn.Write(b)
+	return err
+}
+
 type ServerConfig struct {
 	ListenAddr string
 }
@@ -41,9 +46,27 @@ func (s *Server) Start() {
 		panic(err)
 	}
 
-	fmt.Printf("game server running on port %s", s.ListenAddr)
+	fmt.Printf("game server running on port %s\n", s.ListenAddr)
 
 	s.acceptLoop()
+}
+
+func (s *Server) acceptLoop() {
+	for {
+		conn, err := s.listener.Accept()
+		if err != nil {
+			panic(err)
+		}
+
+		peer := &Peer{
+			conn: conn,
+		}
+
+		s.addPeer <- peer
+		peer.Send([]byte("go-pocker v0.1-alpha"))
+
+		go s.handleConn(conn)
+	}
 }
 
 func (s *Server) handleConn(conn net.Conn) {
@@ -55,17 +78,6 @@ func (s *Server) handleConn(conn net.Conn) {
 		}
 
 		fmt.Println(string(buf[:n]))
-	}
-}
-
-func (s *Server) acceptLoop() {
-	for {
-		conn, err := s.listener.Accept()
-		if err != nil {
-			panic(err)
-		}
-
-		go s.handleConn(conn)
 	}
 }
 
@@ -84,7 +96,7 @@ func (s *Server) loop() {
 	for {
 		select {
 		case peer := <-s.addPeer:
-			fmt.Printf("new player connected %s", peer.conn.RemoteAddr())
+			fmt.Printf("new player connected %s\n", peer.conn.RemoteAddr())
 			s.peers[peer.conn.RemoteAddr()] = peer
 		}
 	}
