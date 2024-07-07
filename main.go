444aa1d68e8dfb382c7627122181b4353b83133e
Hau Tran <tranleconghau772001@gmail.com>
2024-07-07 22:06:58 +0700

    finish ep1

--- main.go
+++ main.go
@@ -1,17 +1,14 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
+	"github.com/marktran77/go-pocker/deck"
 	"github.com/marktran77/go-pocker/p2p"
 )
 
 func main() {
-
-	// for i := 0; i < 10; i++ {
-	// 	d := deck.New()
-	// 	fmt.Println(d)
-	// 	fmt.Println()
-	//}
-
 	cfg := p2p.ServerConfig{
 		Version:    "go-pocker v0.1-alpha",
 		ListenAddr: ":3000",
@@ -19,10 +16,18 @@ func main() {
 	server := p2p.NewServer(cfg)
 	go server.Start()
 
+	time.Sleep(1 * time.Second)
+
 	remoteCfg := p2p.ServerConfig{
 		Version:    "go-pocker v0.1-alpha",
 		ListenAddr: ":4000",
 	}
 	remoteServer := p2p.NewServer(remoteCfg)
-	remoteServer.Start()
+	go remoteServer.Start()
+	if err := remoteServer.Connect(":3000"); err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Print(deck.New())
+	select {}
 }
